7b29471f5fcc5758dd3f70bab40c6582a92d9eca
20172579 <kestutis.dumbrava@stud.vgtu.lt>
2019-03-31 17:34:54 +0300

    Version3

--- main.cpp
+++ main.cpp
@@ -3,144 +3,141 @@
 #include <string>
 #include <fstream>
 #include <iomanip>
+#include <vector>
 using namespace std;
 
+bool knownN = false;
+bool median = false;
+vector<int> vectorius;
+string answer = "";
+
+struct studentas{
+    string name;
+    string surname;
+    int h1;
+    int h2;
+    int h3;
+    int h4;
+    int h5;
+    float exam;
+} student;
+
 void skaitau() {
-    int h1,h2,h3,h4,h5,exam;
-    string name,surname;
     ifstream inFile;
     inFile.open("Students.txt");
     string line;
     inFile>>line;
 
-    if (inFile.is_open())
+    if (inFile.is_open()){
         getline(inFile,line);
-  {
-    while ( getline (inFile,line) )
-    {
-     inFile>>name>>surname>>h1>>h2>>h3>>h4>>h5>>exam;
+    }
+
+    while ( getline (inFile,line) ){
+     inFile>>student.name>>student.surname>>student.h1>>student.h2>>student.h3>>student.h4>>student.h5>>student.exam;
     }
     inFile.close();
-  }
-
-  cout<<name<<surname<<h1<<h2<<h3<<h4<<h5<<exam<<endl;
 }
 
-int blabla(){
-    int a,b;
-    a = 5;
-    b = 7;
-    return a+b;
-}
-
-int main()
-{
-    string name;
-    string surname;
-    int n=1000;
-    float exam = 0.0;
-    bool median = false;
-    float medianSum = 0;
-    string answer;
-    float homeworkSum = 0;
-    bool knownN = false;
-    int tempoI = 0;
-    int home = 0;
-
-
-
-    float averageOfHomework  = 0.0;
-   skaitau();
-
-   int rez;
-   rez = blabla();
-
-cout<<rez<<endl;
-
-cout<<name<<" "<<surname<<endl;
+void credentials(){
     cout<<"Enter your name and surname"<<endl;
-    cin>>name>>surname;
+    cin>>student.name>>student.surname;
+}
 
-    cout<<"Enter exam score"<<endl;
-    cin>>exam;
-
-    cout<<"Do you know homework amount (n), (yes or no)"<<endl;
+void useMedian() {
+    cout<<"Wanna use median?  (yes or no)"<<endl;
     cin>>answer;
-
-    if(answer == "yes" || answer == "Yes" || answer == "YES"){
-        knownN = true;
-        cout<<"Enter n"<<endl;
-        cin>>n;
-        cout<<"Enter grades"<<endl;
-    }
-    int homework[n];
-
-    if(knownN){
-        for(int i = 0; i < n ; i++){
-            cin>>homework[i];
-        }
-    }
-    else {
-        n = 0;
-        while(knownN == false){
-            cout<<"Do you want to enter n? (yes or no)?"<<endl;
-            cin>>answer;
-            if(answer == "yes" || answer == "Yes" || answer == "YES"){
-            cin>>homework[tempoI];
-            tempoI++;
-            n++;
-            }else{
-                knownN = true;
-            }
-        }
-    }
-
-    cout<<"Use median? (yes or no)"<<endl;
-    cin>>answer;
-
     if(answer == "yes" || answer == "Yes" || answer == "YES"){
         median = true;
     }
+}
 
-    if(n % 2 == 0){
-        sort(homework, homework+n);
-        int indicator = n / 2;
-        medianSum= (homework[indicator] + homework[indicator-1])/2;
-    } else{
-        sort(homework, homework+n);
-        int indicator = n / 2;
-        medianSum = homework[indicator];
+void score(){
+    cout<<"Enter exam score"<<endl;
+    cin>>student.exam;
+}
 
-    }
-        for(int i = 0; i< n ; i++){
-            homeworkSum += homework[i];
+void fillHomeworkVector(){
+    int tempo;
+    bool enter = false;
+    cout<<"Enter homework grade"<<endl;
+    cin>>tempo;
+    vectorius.push_back(tempo);
+    while(enter == false){
+        cout<<"Do you want to enter another grade? (yes or no)?"<<endl;
+        cin>>answer;
+        if(answer == "yes" || answer == "Yes" || answer == "YES"){
+            cin>>tempo;
+            vectorius.push_back(tempo);
         }
-        averageOfHomework = homeworkSum / n;
+        else {
+            enter = true;
+        }
+    }
+}
 
-        float final_points = 0.4 * averageOfHomework + 0.6 * exam;
+float medianSum(){
+    float medianSum = 0;
 
+    if(vectorius.size() % 2 == 0){
+        sort(vectorius.begin(), vectorius.end());
+        int indicator = vectorius.size() / 2;
+        medianSum = (vectorius[indicator] + vectorius[indicator-1]) / 2;
+    } else{
+        sort(vectorius.begin(), vectorius.end());
+        int indicator = vectorius.size() / 2;
+        medianSum = vectorius[indicator];
+    }
 
-    if(median){
+    return medianSum;
+  }
+
+float finalPoints() {
+     int homeworkSum = 0;
+     float averageOfHomework = 0;
+     for(int i = 0; i < vectorius.size() ; i++){
+            homeworkSum += vectorius[i];
+        }
+        averageOfHomework = homeworkSum / vectorius.size();
+
+       return 0.4 * averageOfHomework + 0.6 * student.exam;
+}
+
+void printTable(){
+    useMedian();
+    float sum = medianSum();
+    float finals = finalPoints();
+
+  if(median){
 
     cout<<left<<setw(30)<<"Surname";
     cout<<left<<setw(30)<<"Name";
     cout<<left<<setw(30)<<"Final points (Avg.)";
     cout<<left<<setw(30)<<"Final points (Med.)"<<endl;
 
-    cout<<left<<setw(30)<<surname;
-    cout<<left<<setw(30)<<name;
-    cout<<left<<setw(30)<<setprecision(2)<<final_points;
-    cout<<left<<setw(30)<<setprecision(2)<<medianSum<<endl;
+    cout<<left<<setw(30)<<student.surname;
+    cout<<left<<setw(30)<<student.name;
+    cout<<left<<setw(30)<<setprecision(2)<<finals;
+    cout<<left<<setw(30)<<setprecision(2)<<sum<<endl;
 
-    }else{
+    }
+    else{
 
     cout<<left<<setw(30)<<"Surname";
     cout<<left<<setw(30)<<"Name";
     cout<<left<<setw(30)<<"Final points (Avg.)"<<endl;
 
-    cout<<left<<setw(30)<<surname;
-    cout<<left<<setw(30)<<name;
-    cout<<left<<setw(30)<<setprecision(2)<<final_points;
+    cout<<left<<setw(30)<<student.surname;
+    cout<<left<<setw(30)<<student.name;
+    cout<<left<<setw(30)<<setprecision(2)<<finals;
 
     }
-}
+  }
+
+
+int main()
+{
+    credentials();
+    score();
+    fillHomeworkVector();
+    printTable();
+ }
